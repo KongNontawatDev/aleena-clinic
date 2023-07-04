61892aca0368b24322da67d31568520f2942751c
KongNontawatDev <Kns.dev15885@gmail.com>
2023-07-04 21:03:48 +0700

    build 04-07-2023

--- dist/assets/Contact-576486b5.js
+++
@@ -1,1 +0,0 @@
-import{j as t,C as n}from"./index-e496c836.js";function e({}){return t.jsx(t.Fragment,{children:t.jsx(n,{children:t.jsx("div",{children:"Contact"})})})}export{e as default};
--- dist/assets/Service-1a04f5e6.js
+++
@@ -1,1 +0,0 @@
-import{j as e,C as r}from"./index-e496c836.js";function n({}){return e.jsx(e.Fragment,{children:e.jsx(r,{children:e.jsx("div",{children:"Service"})})})}export{n as default};
