04c943cb70840f475251a90e3ba41b47977c5e2a
KongNontawatDev <Kns.dev15885@gmail.com>
2023-07-04 18:48:46 +0700

    edit offset banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -238,7 +238,7 @@ export default function Home({}: Props) {
 								height={"80%"}
 								src={"./image/img1.jpg"}
 										data-aos="fade-left"
-										data-aos-offset="-300"
+										data-aos-offset="-500"
 							/>
 						</Flex>
 					</Stack>
